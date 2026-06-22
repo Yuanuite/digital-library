017b1f2840de11202894bba7821030d9e5776df7
Yuanuite <Yuanuite@users.noreply.github.com>
2026-06-22 10:44:03 +0000

    deploy: a59caebec756a50ebaaafa8d900c5ec8f62b60e1

---
+++ javascripts/mathjax.js
@@ -0,0 +1,10 @@
+MathJax = {
+  tex: {
+    inlineMath: [["$", "$"], ["\\(", "\\)"]],
+    displayMath: [["$$", "$$"], ["\\[", "\\]"]]
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise();
+});
